15526c92236ffa7dfd8adeb676e3ea04e9f33c13
Kyle T. Edwards <kyle@brighthire.ai>
2020-08-16 20:07:24 -0400

    Add input for server IP

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -1,32 +1,49 @@
 import React, { useState } from 'react';
-import { execFile } from "child_process";
+import { execFile, ChildProcess } from "child_process";
 import fs from "fs"
 import { homedir } from "os";
 
 const App = () => {
   const [logs, setLogs] = useState("");
+  const [serverIP, setServerIP] = useState("");
+  const [child, setChild] = useState<ChildProcess | null>(null);
 
   const connect = () => {
-    const jackdConfig = "/usr/local/bin/jackd -dcoreaudio -r48000 -p256";
-    fs.writeFile(`${homedir()}/.jackdrc`, jackdConfig, function(err) {
-      if(err) {
-          throw new Error(err.message);
-      }
-      console.log("Config updated.");
-    }); 
-    const child = execFile("/usr/local/bin/jacktrip", ["-C", "34.86.239.114"]);
-    child.stdout.setEncoding('utf8');
-    child.stdout.on('data', (data) => {
+    // const jackdConfig = "/usr/local/bin/jackd -dcoreaudio -r48000 -p256";
+    // fs.writeFile(`${homedir()}/.jackdrc`, jackdConfig, function(err) {
+    //   if(err) {
+    //       throw new Error(err.message);
+    //   }
+    //   console.log("Config updated.");
+    // });
+    console.log(serverIP)
+    const c = execFile("/usr/local/bin/jacktrip", [`-C${serverIP}`, "-n1", "-q8", "--rtaudio", "--bufsize=256"]);
+    setChild(c);
+    c.stdout.setEncoding("utf-8");
+    c.on("exit", () => {
+      setChild(null);
+      setLogs("");
+    });
+    c.stdout.on("data", (data: string) => {
+      setLogs(`${data}\n${logs}`);
       console.log(data);
-      data = data.toString();
-      setLogs(`${data}\n${logs}`)
+      if (data.includes("UDP WAITED MORE THAN")) {
+        c.kill()
+      }
     });
   };
-  
+
   return (
     <div style={{backgroundColor: "white"}}>
-      <h1>Welcome to JackJam!</h1>
-      <button onClick={() => connect()}>Connect</button>
+      <h1>JackJam</h1>
+      {child ? (
+        <button onClick={() => child.kill()}>Disconnect</button>
+      ) : (
+        <>
+          <input value={serverIP} onChange={(e) => setServerIP(e.currentTarget.value)} />
+          <button onClick={() => connect()} disabled={!serverIP}>Connect</button>
+        </>
+      )}
       <div>
         {logs}
       </div>
