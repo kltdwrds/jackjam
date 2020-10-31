300c34116af9ab883510b3f2757f735b8162b818
Kyle T. Edwards <kyle@brighthire.ai>
2020-10-31 03:26:18 -0400

    Build sign and notarize

---
+++ build/notarize.js
@@ -0,0 +1,26 @@
+const { notarize } = require('electron-notarize');
+try {
+  require('dotenv').config();
+} catch (error) {
+  console.log(error)
+}
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+  const appPath = `${appOutDir}/${appName}.app`;
+  const appId = require('../package.json').build.appId;
+
+  console.log(`Notarizing ${appId}...`)
+
+  return await notarize({
+    appBundleId: appId,
+    appPath: appPath,
+    appleId: process.env.AC_USERNAME,
+    appleIdPassword: process.env.AC_PASSWORD,
+  });
+};
